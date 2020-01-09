43007edba3ac8764270b593db38557ebf199e42d
Jefferson <Jefferson@DESKTOP-ADBK4I5>
2020-01-09 08:02:00 -0300

    Aula 2/4 - Implementando o serviço Fornecedor

---
+++ src/main/java/br/com/alura/microservice/fornecedor/FornecedorMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.microservice.fornecedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FornecedorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FornecedorMsApplication.class, args);
+	}
+
+}
